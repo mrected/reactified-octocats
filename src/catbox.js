b51fd162eb73ea2d9f82ad828024881d70085f80
Chris Terrazas <chris.terrazas@gmail.com>
2018-11-13 15:07:42 -0500

    pulls data from a json file

--- src/catbox.js
+++ src/catbox.js
@@ -1,65 +1,28 @@
 import React, { Component } from 'react'
+import Octocats from './cats.json'
 class Catbox extends Component {
   render() {
+    let cats = Octocats
+    console.log(cats)
     return (
       <section id="cat-block">
-        <div class="picture-box">
-          <div class="cats">
-            <img
-              src="https://octodex.github.com//images/Sentrytocat_octodex.jpg"
-              alt="Sentrytocat"
-            />
+        {cats.map(cat => (
+          <div class="picture-box">
+            <div class="cats">
+              <img src={cat.image} alt={cat.name} />
+            </div>
+            <div class="caption">
+              <div class="down-arrow" />
+              <p class="pic-number">{cat.number}</p>
+              <p>
+                the <span class="caption-title">{cat.name}</span> by
+              </p>
+              {cat.authors.map(author => (
+                <img src={author.image} alt="creator image" />
+              ))}
+            </div>
           </div>
-          <div class="caption">
-            <div class="down-arrow" />
-            <p class="pic-number">#145</p>
-            <p>
-              the <span class="caption-title">Sentrytocat</span> by
-            </p>
-            <img
-              src="https://github.com/cameronmcefee.png"
-              alt="creator image"
-            />
-          </div>
-        </div>
-        <div class="picture-box">
-          <div class="cats">
-            <img
-              src="https://octodex.github.com//images/Sentrytocat_octodex.jpg"
-              alt="Sentrytocat"
-            />
-          </div>
-          <div class="caption">
-            <div class="down-arrow" />
-            <p class="pic-number">#145</p>
-            <p>
-              the <span class="caption-title">Sentrytocat</span> by
-            </p>
-            <img
-              src="https://github.com/cameronmcefee.png"
-              alt="creator image"
-            />
-          </div>
-        </div>
-        <div class="picture-box">
-          <div class="cats">
-            <img
-              src="https://octodex.github.com//images/Sentrytocat_octodex.jpg"
-              alt="Sentrytocat"
-            />
-          </div>
-          <div class="caption">
-            <div class="down-arrow" />
-            <p class="pic-number">#145</p>
-            <p>
-              the <span class="caption-title">Sentrytocat</span> by
-            </p>
-            <img
-              src="https://github.com/cameronmcefee.png"
-              alt="creator image"
-            />
-          </div>
-        </div>
+        ))}
       </section>
     )
   }
